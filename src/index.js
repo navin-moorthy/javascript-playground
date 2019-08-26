e9803d3dd9356b5047f1a5921269759ce33c2738
NavinNavi19 <navin007.a@gmail.com>
2019-08-26 05:55:55 +0000

    Update deps

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '../assets/css/style.css';
+import "../assets/css/style.css";
 
-const app = document.getElementById('app');
-app.innerHTML = '<h1>JavaScript Basics</h1>';
+const app = document.getElementById("app");
+app.innerHTML = "<h1>JavaScript Basics</h1>";
